e1e7349d0179bf4c541d6fc7e1811bbd46ca6193
Ntuthuko Jr Mashinini <ntuthuko@ossconsult.com>
2022-02-08 12:59:32 +0200

    update alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,17 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
-  * print_alphabet - Make the alphabet
-  *
-  * Return: void
-  */
+ *print_alphabet - Make the alphabet
+ *
+ *Return: void
+ *
+ */
 void print_alphabet(void)
 {
-	char c;
+char i;
 
-	for (c = 'a'; c <= 'z'; c++)
-	{
-		_putchar(c);
-	}
-
-	_putchar('\n');
+for (i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
 }
