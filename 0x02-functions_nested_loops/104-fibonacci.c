9f5203d12d2706e3e422512430a9676ac66f570a
Ntuthuko Jr Mashinini <ntuthuko@ossconsult.com>
2022-02-08 13:29:58 +0200

    update

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,47 +1,31 @@
-#include <limits.h>
-#include <math.h>
 #include <stdio.h>
-
 /**
- * main - entry point
+ * main - main function
  *
- * Return: Always 0.
+ * Return: nothing
  */
 int main(void)
 {
-	int i;
-	int j;
-	long a_lo = 1;
-	long b_lo = 2;
-	long a_hi = 0;
-	long b_hi = 0;
-	int limit_len = floor(log10(LONG_MAX / 2));
-	long limit = pow(10, limit_len);
+int counter = 2;
 
-	for (i = 0; i < 98; ++i)
-	{
-		if (a_hi)
-		{
-			printf("%ld", a_hi);
-			for (j = floor(log10(a_lo)) + 1; j < limit_len; ++j)
-				putchar('0');
-		}
-		printf("%ld", a_lo);
-		b_lo = b_lo + a_lo;
-		a_lo = b_lo - a_lo;
-		a_hi = b_hi - a_hi;
-		b_hi = b_hi + a_hi;
-		if (b_lo >= limit)
-		{
-			b_hi += b_lo / limit;
-			b_lo %= limit;
-			a_hi += a_lo / limit;
-			a_lo %= limit;
-		}
-		if (i < 97)
-			printf(", ");
-	}
-	putchar('\n');
+float a = 1;
+float b = a + 1;
+float c = a + b;
 
-	return (0);
+printf("%.0f, ", a);
+printf("%.0f, ", b);
+while (counter < 98)
+{
+counter++;
+printf("%.0f", c);
+a = b;
+b = c;
+c = a + b;
+if (counter < 98)
+{
+printf(", ");
+}
+}
+printf("\n");
+return (0);
 }
