5d8182f3212ae2bdc72daf675148d8142071839a
Singular Learner 9 <SSLearner9@SINGULAR.CO.ZA>
2022-02-10 13:33:26 +0200

    Commit changes

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,46 +1,47 @@
 #include "main.h"
 
 /**
- * print_number -  checks for checks for a digit (0 through 9).
- * @n: n -  Variable
- * Return: Always 0.
+ * print_number - Function that prints an integer.
+ * @n: int type number
+ * Description: Can only use _putchar to print.
  */
 void print_number(int n)
 {
-	unsigned int z;
-	int m, b;
+	long m; /* power of 10 */
+	int c; /* boolean check */
+	long num; /* convert int to long */
 
-	b = 10;
-
-	if (n < 10 && n >= 0)
+	num = n;
+	/* negatives */
+	if (num < 0)
 	{
-		_putchar (n + '0');
-	}
-	else if (n > -10 && n < 0)
-	{
-		n = n - 2 * n;
+		num *= -1;
 		_putchar('-');
-		_putchar (n + '0');
 	}
 
-	else
+	/* count up */
+	m = 1;
+	c = 1;
+	while (c)
 	{
-		if (n < 0)
+		if (num / (m * 10) > 0)
+			m *= 10;
+		else
+			c = 0;
+	}
+
+	/* count down */
+	while (num >= 0)
+	{
+		if (m == 1)
 		{
-			n = n * -1;
-			_putchar ('-');
+			_putchar(num % 10 + '0');
+			num = -1;
 		}
-		z = n;
-	while (z / b > 9)
-	{
-		b = b * 10;
-	}
-	while (b > 0)
-	{
-		m = z / b;
-		z = z % b;
-		_putchar (m + '0');
-		b = b / 10;
-	}
+		else
+		{
+			_putchar((num / m % 10) + '0');
+			m /= 10;
+		}
 	}
 }
