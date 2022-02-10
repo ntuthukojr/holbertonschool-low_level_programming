7244c26401096c0424167efc966593be51bc73c6
Singular Learner 9 <SSLearner9@SINGULAR.CO.ZA>
2022-02-10 13:45:28 +0200

    Commit changes

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,34 +1,21 @@
 #include "main.h"
 
 /**
- * print_number - prints an integer on console
- * @n: number to be printed
- *
- * Return: void, and the stated print
+ * print_number - Prints an integer.
+ * @n: The integer to be printed.
  */
-
 void print_number(int n)
 {
+	unsigned int num = n;
+
 	if (n < 0)
+	{
 		_putchar('-');
-	print_any_int(n);
-}
+		num = -num;
+	}
 
-/**
- * print_any_int - uses _putchar to print every digit of any int
- * @m: input integer to be printed with _putchar
- *
- * Return: void, printing every digit of m into stdout
- */
-void print_any_int(int m)
-{
-	int last;
+	if ((num / 10) > 0)
+		print_number(num / 10);
 
-	if (m / 10)
-		print_any_int(m / 10);
-
-	last = m % 10;
-	if (last < 0)
-		last = last * -1;
-	_putchar(last + '0');
+	_putchar((num % 10) + '0');
 }
