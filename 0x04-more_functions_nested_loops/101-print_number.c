2e4e2092d46d179dc706bb5c94a2ad7c34f6ae39
Singular Learner 9 <SSLearner9@SINGULAR.CO.ZA>
2022-02-10 13:36:22 +0200

    Commit changes

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,47 +1,32 @@
 #include "main.h"
 
 /**
- * print_number - Function that prints an integer.
- * @n: int type number
- * Description: Can only use _putchar to print.
+ * print_number - takes an integer and prints it to the screen
+ * @n: number to be printed
+ *
  */
+
 void print_number(int n)
 {
-	long m; /* power of 10 */
-	int c; /* boolean check */
-	long num; /* convert int to long */
+	int digit, count, temp;
 
-	num = n;
-	/* negatives */
-	if (num < 0)
-	{
-		num *= -1;
+	digit = n;
+	count = 0;
+
+	if (digit < 0)
 		_putchar('-');
-	}
-
-	/* count up */
-	m = 1;
-	c = 1;
-	while (c)
+	for (temp = 1; digit > 9 || digit < -9; temp *= 10)
 	{
-		if (num / (m * 10) > 0)
-			m *= 10;
-		else
-			c = 0;
+		digit /= 10;
+		count++;
 	}
-
-	/* count down */
-	while (num >= 0)
+	for (digit = n; count >= 0; count--)
 	{
-		if (m == 1)
-		{
-			_putchar(num % 10 + '0');
-			num = -1;
-		}
+		if (digit / temp < 0)
+			_putchar(((digit / temp) * -1) + '0');
 		else
-		{
-			_putchar((num / m % 10) + '0');
-			m /= 10;
-		}
+			_putchar(digit / temp + '0');
+		digit %= temp;
+		temp /= 10;
 	}
 }
