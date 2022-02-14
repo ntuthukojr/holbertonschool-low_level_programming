326b0b9365d9e4fbc8dac42ddd3f3bb38dd88d4f
Singular Learner 9 <SSLearner9@SINGULAR.CO.ZA>
2022-02-14 10:01:11 +0200

    apply reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,31 +1,21 @@
 #include "main.h"
+
 /**
- * rev_string - reverses a string
- * @s: holi
- *
+ * rev_string - Reverses a string.
+ * @s: The string to be reversed.
  */
 void rev_string(char *s)
 {
-	int i;
-	int j;
-	int temp;
-	int length;
+	int len = 0, index = 0;
+	char tmp;
 
-	while (s[i] != 0)
+	while (s[index++])
+		len++;
+
+	for (index = len - 1; index >= len / 2; index--)
 	{
-		i++;
-	}
-
-	length = i;
-	i = 0;
-	j = length - 1;
-
-	while (i < j)
-	{
-		temp = s[i];
-		s[i] = s[j];
-		s[j] = temp;
-		i++;
-		j--;
+		tmp = s[index];
+		s[index] = s[len - index - 1];
+		s[len - index - 1] = tmp;
 	}
 }
