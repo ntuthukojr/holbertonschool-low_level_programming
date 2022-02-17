9cda745b9b05b3f10c554fb64f42454a992a96d3
Ntuthuko Jr Mashinini <ntuthuko@ossconsult.com>
2022-02-17 16:18:26 +0200

    update task 5

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -2,48 +2,46 @@
 #include <stdio.h>
 
 /**
- *print_buffer - Print a buffer 10 bytes per line.
- *@b: Buffer address.
- *@size: Number of characters to be printed.
+ * print_buffer - print a buffer
+ * @b: the buffer to print
+ * @size: the number of bytes to print
+ *
+ * Description: This function prints the content of of size bytes of the
+ * buffer pointed to by b. 10 bytes are printed per line, with each line
+ * prefixed with the position of the first byte, starting at 0. Each line
+ * shows the heaxadecimal content of the buffer 2 bytes at a time, with
+ * each pair separated by a space, followed by the content of the buffer
+ * with non-printable characters shown as a `.'.
+ *
+ * Return: void
  */
+
 void print_buffer(char *b, int size)
 {
-	int i, pi, c;
+	int b_pos;
+	int l_pos;
 
-	if (size <= 0)
-		printf("\n");
-	else
+	for (b_pos = 0; b_pos < size; b_pos += 10)
 	{
-		for (i = 9, pi = 0; i < size; i += 10)
-		{	printf("%.8x: ", pi);
-				for (c = pi; c <= i ; c++)
-				{	printf("%.2x", b[c]);
-					if (c % 2 == 1)
-						printf(" ");	}
-				for (c = pi; c <= i ; c++)
-				{
-					if (b[c] < 32)
-						printf(".");
-					else
-						printf("%c", b[c]);	}
-					printf("\n");
-					pi = i + 1;	}
-		if (pi < size - 1)
-		{	i -= 9;
-			printf("%.8x: ", i);
-			for (c = i; c < i + 10; c++)
-			{
-				if (c < size)
-					printf("%.2x", b[c]);
-				else
-					printf("  ");
-				if (c % 2 == 1)
-					printf(" ");	}
-			for (; i < size ; i++)
-			{
-				if (b[i] < 32)
-					printf(".");
-				else
-					printf("%c", b[i]);
-			}
-			printf("\n");	}	}	}
+		printf("%08x: ", b_pos);
+		for (l_pos = 0; l_pos < 10; ++l_pos)
+		{
+			if (b_pos + l_pos < size)
+				printf("%02x", b[b_pos + l_pos]);
+			else
+				printf("  ");
+			if (l_pos % 2)
+				putchar(' ');
+		}
+		for (l_pos = 0; l_pos < 10 && b_pos + l_pos < size; ++l_pos)
+		{
+			if (b[b_pos + l_pos] < 32 || b[b_pos + l_pos] > 126)
+				putchar('.');
+			else
+				putchar(b[b_pos + l_pos]);
+		}
+		if (b_pos + l_pos < size)
+			putchar('\n');
+	}
+	putchar('\n');
+}
