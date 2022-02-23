46033e694af2a4b0e18dbae03ad38281b16243dd
Singular Learner 9 <SSLearner9@SINGULAR.CO.ZA>
2022-02-23 12:00:27 +0200

    push Task 7

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,17 +1,53 @@
 #include "main.h"
 /**
- * is_palindrome - find a palindrome string
- * @s: string for check
- *
- * Return: 1 if is palindrome, otherwise 0
+ * lenght - size of string
+ * @s: initial string
+ * Return: lenght
+ */
+int lenght(char *s)
+{
+	if (*s != '\0')
+	{
+		s++;
+		return (1 + lenght(s));
+	}
+	else
+	{
+		return (0);
+	}
+}
+
+/**
+ * comp - compare two chars
+ * @s: string
+ * @size: lenght of s
+ * @inicio: variable to go through the string
+ * Return: 1 or 0 if it's palindrome
+ */
+int comp(char *s, int size, int inicio)
+{
+	if (s[inicio] == s[size - 1 - inicio])
+	{
+		if (inicio <= (size / 2))
+		{
+			return (comp(s, lenght(s), inicio + 1));
+		}
+		else
+		{
+			return (1);
+		}
+	}
+	else
+	{
+		return (0);
+	}
+}
+/**
+ * is_palindrome - gives if it is prime number
+ * @s: number
+ * Return: 1 if is palindrome or 0 if not
  */
 int is_palindrome(char *s)
 {
-	int i;
-
-	i = 0;
-
-	if (*s == i)
-		return (0);
-	return (1);
+	return (comp(s, lenght(s), 0));
 }
