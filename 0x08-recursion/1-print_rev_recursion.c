579781f34623666d86177b6eda885897d2a60c59
Singular Learner 9 <SSLearner9@SINGULAR.CO.ZA>
2022-02-23 09:21:36 +0200

    push Task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,14 +2,14 @@
 /**
  * _print_rev_recursion - prints a string in reverse
  * @s: the pointer input
- * 
+ *
  */
 void _print_rev_recursion(char *s)
 {
-    if (*s != '\0')
-    {
-            _print_rev_recursion(s + 1);
-            _putchar(*s);
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 
-    }
+	}
 }
