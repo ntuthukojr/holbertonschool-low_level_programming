dff24d8fa5de26591445b921c029bb937b091299
Singular Learner 9 <SSLearner9@SINGULAR.CO.ZA>
2022-03-07 14:17:45 +0200

    function like macro

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,6 @@
-#ifndef absolute_value
-#define absolute_value
-#define ABS(x) (((x) < 0) ? (x) * (-1) : (x))
+#ifndef _FUNCTION_LIKE_MACRO_
+#define _FUNCTION_LIKE_MACRO_
+
+#define ABS(x) ((x) < (0) ? ((x) * (-1)) : (x))
+
 #endif
