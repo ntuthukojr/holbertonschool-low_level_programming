eecb47f828ac4b696283e84aed836ae0b669de00
Singular Learner 9 <SSLearner9@SINGULAR.CO.ZA>
2022-03-07 10:41:50 +0200

    function that prints a struct dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_dog -- a function that prints a struct dog
+ *
+ * @d: incoming parameter
+ *
+ *
+ * Return: void (Success)
+ *
+ */
+
+void print_dog(struct dog *d)
+{
+
+
+
+return (void);
+
+}
