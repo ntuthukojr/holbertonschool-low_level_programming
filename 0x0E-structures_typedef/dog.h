caa52a99999cc7135f549a3fffc6a47c4f0d756a
Singular Learner 9 <SSLearner9@SINGULAR.CO.ZA>
2022-03-07 10:34:34 +0200

    Define a new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,24 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+/**
+ * struct dog - a struct demo
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Project #E/0
+ */
+
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+#endif
