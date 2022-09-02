08396cbd38aee9213f0c5f4030f718df37b35562
Ntuthuko Jr Mashinini <ntuthuko@ossconsult.com>
2022-09-02 11:47:45 +0200

    Add Files

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,21 +1,27 @@
 #include "search_algos.h"
+
 /**
- * linear_search - search using a linear search
- * @array: the array
- * @size: size of the array
- * @value: the search value
- * Return: index of first instance of value or -1 on fail
+ * linear_search - searches for a value in array using the Linear search algo
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in array
+ * @value: value to search for
+ *
+ * Return: first index where value is located, or -1 on failure
  */
 int linear_search(int *array, size_t size, int value)
 {
-	int i = 0;
+	size_t i;
+	int tmp;
 
-	while ((size_t) i < size)
+	if (array != NULL)
 	{
-		printf("Value checked array[%d] = [%d]\n", i, array[i]);
-		if (array[i] == value)
-			return (i);
-		i++;
+		for (i = 0; i < size; i++)
+		{
+			tmp = array[i];
+			printf("Value checked array[%lu] = [%d]\n", i, tmp);
+			if (tmp == value)
+				return (i);
+		}
 	}
 	return (-1);
 }
--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,38 +1,59 @@
 #include "search_algos.h"
+int recursive_binary_search(int *array, size_t start, size_t end, int value);
+
 /**
- * binary_search - search using a binary search
- * @array: the array
- * @size: array size
- * @value: the search value
- * Return: where the value is located or -1 on fail
+ * binary_search - Searches for a value in a sorted array of integers
+ *	using the Binary search algorithm.
+ *
+ * @array: The array of integers.
+ * @size: The size of the array.
+ * @value: The value to search.
+ *
+ * Return: The index where value is located.
  */
 int binary_search(int *array, size_t size, int value)
 {
-	int low = 0, middle, high, index;
+	size_t index = 0;
 
-	if (size == 0 || array == NULL)
+	if (array == NULL)
 		return (-1);
 
-	high = size - 1;
+	index = recursive_binary_search(array, index, size - 1, value);
 
-	while (low <= high)
-	{
-		printf("Searching in array: ");
-		for (index = low; index <= high; index++)
-			printf(index < high ? "%d, " : "%d\n", array[index]);
-		middle = (high + low) / 2;
-		if (array[middle] < value)
-		{
-			low = middle + 1;
-		}
-		else if (array[middle] > value)
-		{
-			high = middle - 1;
-		}
-		else
-		{
-			return (middle);
-		}
-	}
+	return (index);
+}
+
+/**
+ * recursive_binary_search - Searches using the Binary search algorithm.
+ *
+ * @array: The array of integers.
+ * @start: The start of the array.
+ * @end: The end of the array.
+ * @value: The value to search.
+ *
+ * Return: The index where value is located.
+ */
+int recursive_binary_search(int *array, size_t start, size_t end, int value)
+{
+	size_t middle = 0, i = 0;
+
+	printf("Searching in array: %d", array[start]);
+	for (i = 1 + start; i <= end; i++)
+		printf(", %d", array[i]);
+	printf("\n");
+
+	if (start == end && value != array[start])
+		return (-1);
+
+	middle = ((end - start) / 2) + start;
+
+	if (array[middle] == value)
+		return (middle);
+
+	if (array[middle] > value)
+		return (recursive_binary_search(array, start, middle - 1, value));
+	if (array[middle] < value)
+		return (recursive_binary_search(array, middle + 1, end, value));
+
 	return (-1);
 }
